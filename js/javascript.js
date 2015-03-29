9b235b3bd7866d0e454eb8e0557b548b7d4b155e
Nicolas PERU <nicolas.peru@gmail.com>
2015-03-29 22:44:21 +0200

    Returning tile on click

--- js/javascript.js
+++ js/javascript.js
@@ -1,16 +1,38 @@
-function MyFirstMemory	() {
+var MyFirstMemory = function() {
 var body = document.body;
-var card = document.createElement('div')
-card.setAttribute("class", "flip-container");
-card.setAttribute("ontouchstart", "this.classList.toggle('hover');");
-card.innerHTML='<div class="flipper">'+
-'		<div class="front">'+
-'		</div>'+
-'		<div class="back">'+
-'		</div>'+
-'	</div>'+
-'</div>';
-
-body.appendChild(card);
+var rows = 4;
+var cols = 3;
+for (var r = 0; r < rows; r++) {
+	var row = document.createElement('span');
+	row.setAttribute("class", "row");
+	for (var c = 0; c < cols; c++) {
+        
+		var card = document.createElement('div')
+		card.setAttribute("class", "flip-container");
+		card.setAttribute("onClick", "MyFirstMemory.flip(this)");
+		card.innerHTML='<div class="flipper">'+
+		'		<div class="front">'+
+		'		</div>'+
+		'		<div class="back">'+
+		'		</div>'+
+		'	</div>'+
+		'</div>';
+		row.appendChild(card);
+	}
+	body.appendChild(row);
 }
-MyFirstMemory();
+
+var flip = function(node) {
+	console.log(node.classList);
+	node.classList.toggle("flip");
+	
+}
+
+var oPublic =
+    {
+      flip: flip,
+    };
+return oPublic;
+
+}();
+
